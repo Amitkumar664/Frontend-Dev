92fd904b3c67aa7a59f884898de203cdd4c17f4e
Amit Kumar <amitkumar4102004b@gmail.com>
2025-11-12 17:26:15 +0530

    added some basics of js

---
+++ Lec1JS/rediclare.js
@@ -0,0 +1,11 @@
+let temp=19;
+var temp2=20;
+const temp3=21;
+
+// rediclare and reinitialize
+// let temp=25;
+var temp2=30;
+// temp3=35; // error2
+console.log(temp);
+console.log(temp2);
+console.log(temp3); 
