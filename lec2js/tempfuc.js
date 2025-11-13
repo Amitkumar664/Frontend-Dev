b1fe0ccfbaf13659f751b270cae056b6589f2372
Amit Kumar <amitkumar4102004b@gmail.com>
2025-11-13 13:14:06 +0530

    added test and date

---
+++ lec2js/tempfuc.js
@@ -0,0 +1,14 @@
+function fn(){
+    var temp=10;
+    console.log(temp);
+}   
+fn();
+
+var myfn=function(){
+    var temp2=20;
+    console.log(temp2);
+}
+myfn();
+
+var myfun2=()=>console.log(30);
+myfun2();
