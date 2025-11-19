fc4b96dfa2fdb99f33f2e636d21c7f7b55ae332e
Amit Kumar <amitkumar4102004b@gmail.com>
2025-11-19 17:03:43 +0530

    added some files

---
+++ lec4js/prototype.js
@@ -0,0 +1,17 @@
+let arr=["aakash","amol"];
+
+let object = {
+    name: "amit",
+    city: "pune",
+    getIntro: function() {
+        console.log(this.name + " belong to " + this.city);
+    }
+};
+
+let object2 = {
+    name: "amol",
+};
+
+object2.__proto__ = object;
+
+object2.getIntro(); // Output: "amol belong to pune"
