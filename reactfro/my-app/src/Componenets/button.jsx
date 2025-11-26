927e757d2eaa27ab0de5a13985153ad43b85ee18
Amit Kumar <amitkumar4102004b@gmail.com>
2025-11-26 15:48:37 +0530

    added some files

---
+++ reactfro/my-app/src/Componenets/button.jsx
@@ -0,0 +1,14 @@
+// rafce
+ReadableStreamDefaultController
+
+import React from 'react';
+
+const button = () => {  
+    return (
+    <div className='app'>
+        <button>Click Me</button>
+    </div>
+  )
+}
+
+export default button;
